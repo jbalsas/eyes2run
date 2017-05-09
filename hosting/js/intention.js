73f87d29901c48af46b36bae8199c45830114e5c
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2017-05-09 12:20:48 +0200

    intentions screen

--- hosting/js/intention.js
+++ hosting/js/intention.js
@@ -1,20 +1,27 @@
+var marker;
+var currentPosition;
+
 function initData() {
 	//get todat startdate and time
 	//
-	myMap();
+
 }
 
-function myMap() {
+function initMap() {
     var mapOptions = {
-        center: new google.maps.LatLng(51.5, -0.12),
-        zoom: 10,
+        center: new google.maps.LatLng(40.4893538, -3.6827461),
+        zoom: 16,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     }
 	var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+	marker = new google.maps.Marker({
+      position:  new google.maps.LatLng(40.4893538, -3.6827461),
+      map: map
+    });
 }
 
 function addIntention(event) {
-	debugger;
 	var currentUser = WeDeploy.auth('http://<serviceID>.<projectID>.wedeploy.io').currentUser;
 
 	if (currentUser) {
@@ -23,9 +30,11 @@ function addIntention(event) {
 	    var time = intention.time.value;
 	    var minutes = intention.minutes.value;
 	    var location = intention.location.value;
+	    var lat = marker.position.lat;
+	    var lng = marker.position.lat;
 
-
-	    //TODO conectar con wedploy
+	    //TODO conectar con wedploy y redirigir a findings.html
+	    location.href = 'findings.html'
 
 	} else {
 		location.href = 'signup.html'
