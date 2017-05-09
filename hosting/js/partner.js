5e33e784ea3e2dd773097b83777b530c828e380e
Chema Balsas <jose.balsas@liferay.com>
2017-05-09 16:57:05 +0200

    adds avatars

--- hosting/js/partner.js
+++ hosting/js/partner.js
@@ -3,7 +3,20 @@ var auth = WeDeploy.auth('auth.eyes2run.wedeploy.me');
 var currentUser = auth.currentUser;
 
 var ariaContainer = document.querySelector('#ariaContainer');
+var partnerContainer = document.querySelector('#partnerContainer');
 var ariaMsg = ariaContainer.innerHTML;
+
+var partnerId = /\?(.*)=(.*)/.exec(location.search)[2];
+var gender = (parseInt(partnerId) % 2) ? 'men' : 'women';
+var avatarId = partnerId.substring(0, 2);
+
+var avatar = new Image();
+avatar.onload = function() {
+    partnerContainer.appendChild(avatar);
+};
+
+avatar.className = 'avatar';
+avatar.src = `https://randomuser.me/api/portraits/${gender}/${avatarId}.jpg`;
 
 setTimeout(function() {
     ariaContainer.innerHTML = 'Your partner for today is Juan';
