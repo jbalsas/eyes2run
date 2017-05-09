c75352aebeb21358ca740bcdb10de446fee8ad5d
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2017-05-09 13:48:31 +0200

    google maps improvments

--- hosting/js/intention.js
+++ hosting/js/intention.js
@@ -1,24 +1,34 @@
 var marker;
-var currentPosition;
+var position = {
+	lat: 40.4763066,
+	lng: -3.6878928
+};
 
 function initData() {
 	var date = new Date();
 
 	document.getElementById('startDate').value = date.toISOString().slice(0,10);
 	document.getElementById('time').value = date.getHours() + ':' + date.getMinutes();
+
+	initMap();
 }
 
 function initMap() {
     var mapOptions = {
-        center: new google.maps.LatLng(40.4893538, -3.6827461),
+        center: new google.maps.LatLng(position.lat, position.lng),
         zoom: 16,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     }
 	var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
 	marker = new google.maps.Marker({
-      position:  new google.maps.LatLng(40.4893538, -3.6827461),
+      position:  new google.maps.LatLng(position.lat, position.lng),
       map: map
+    });
+
+    google.maps.event.addListener(map, 'dblclick', function(e) {
+        var positionDoubleclick = e.latLng;
+        marker.setPosition(positionDoubleclick);
     });
 }
 
@@ -31,11 +41,8 @@ function addIntention(event) {
 	    var startDate = intention.startDate.value;
 	    var time = intention.time.value;
 	    var minutes = intention.minutes.value;
-	    var location = intention.location.value;
 	    var lat = marker.position.lat;
 	    var lng = marker.position.lat;
-
-	    debugger;
 
 	    //TODO conectar con wedploy y redirigir a findings.html
 	    location.href = 'finding.html';
@@ -44,5 +51,3 @@ function addIntention(event) {
 		location.href = 'signup.html'
 	}
 }
-
-initData();
